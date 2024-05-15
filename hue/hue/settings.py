9c3b7c0535fce84f858aea6dcf931fd31806a040
sruthijaganmohan <sruthi12jagan@gmail.com>
2024-05-15 22:31:31 +0530

    vercel deploy

--- hue/hue/settings.py
+++ hue/hue/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!=o5j6kgud@2cib8ya=itgbw)6gdohm36&z4p5a&&f262_h)+*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
